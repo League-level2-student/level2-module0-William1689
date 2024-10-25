d1a9c6e7065e5dd2fd5e725e6c73d29e442d3299
league <league@imac-34.attlocal.net>
2024-10-25 16:57:07 -0700

    I made a robot racing game

---
+++ src/arrays/RobotWithLap.java
@@ -0,0 +1,7 @@
+package arrays;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotWithLap extends Robot { 
+	int lap = 0;
+}
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -7,43 +7,65 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
-	//1. make a main method
+	// 1. make a main method
 	public static void main(String[] args) {
-	int count = 0;	
-	int finish = 1;
+		int count = 0;
+		int finish = 1;
 	
-	//2. create an array of 5 robots.
-	Robot[] robots	= new Robot[5];
-	//3. use a for loop to initialize the robots.
-	for(int i = 0;i<robots.length;i++) {
-		robots[i] = new Robot();
-		robots[i].setX(i*100+200);
-		robots[i].setY(585);
-		robots[i].setSpeed(10);
-		
-	
-	}
-		//4. make each robot start at the bottom of the screen, side by side, facing up
-	
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
 
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-	while(count<finish) {
-		Random random = new Random();
-		Random angle = new Random();
-		for(int i = 0;i<robots.length;i++) {
-			robots[i].move(random.nextInt(50));
-			if(robots[i].getY() <= 0) {
-				count++;
-				JOptionPane.showMessageDialog(null, "WE HAVE A WINNER");
+		// 2. create an array of 5 robots.
+		RobotWithLap[] robots = new RobotWithLap[12];
+		// 3. use a for loop to initialize the robots.
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new RobotWithLap();
+			robots[i].setX(795);
+			robots[i].setY(585);
+			robots[i].setSpeed(50);
+
+		}
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
+
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
+
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
+		int winner = 0;
+		while (count < finish) {
+			Random random = new Random();
+			Random angle = new Random();
+			for (int i = 0; i < robots.length; i++) {
+				robots[i].move(random.nextInt(50));
+				if(robots[i].getX()<=800 && robots[i].getX()>=750 && robots[i].getY()<=45 && robots[i].getY()>=0) {
+					robots[i].turn(-90);
+					robots[i].move(50);
+				}
+					if(robots[i].getX()<=50 && robots[i].getX()>=0 && robots[i].getY()<=40 && robots[i].getY()>=5) {
+						robots[i].turn(-90);
+						robots[i].move(50);
+				}
+					if(robots[i].getX()<=50 && robots[i].getX()>=0 && robots[i].getY()<=595 && robots[i].getY()>=550) {
+						robots[i].turn(-90);
+						robots[i].move(50);
+				}
+				
+				if (robots[i].getX() >= 800 && robots[i].getY()>=550) {
+					robots[i].lap++;
+					robots[i].turn(-90);
+					robots[i].move(100);
+				}
+				if(robots[i].lap == 3) {
+					JOptionPane.showMessageDialog(null, "Robot " + i + " has won!");
+					System.exit(0);
+				}
+
 			}
 		}
+		// 7. declare that robot the winner and throw it a party!
+
+		// 8. try different races with different amounts of robots.
+
+		// 9. make the robots race around a circular track.
+
 	}
-	//7. declare that robot the winner and throw it a party!
-
-	//8. try different races with different amounts of robots.
-
-	//9. make the robots race around a circular track.
-}
 }
